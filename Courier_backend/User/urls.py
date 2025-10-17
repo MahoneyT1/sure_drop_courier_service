a525aa11656f16497b582a44a1a611ab11ce7fd2
MahoneyT1 <ifeanyitech15@gmail.com>
2025-10-17 08:49:36 +0100

    implemented health check view for default api successsful when server is responding

--- Courier_backend/User/urls.py
+++ Courier_backend/User/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from User import views
-from .views import LoginView, LogoutView, VerifyLoginState, HealthCheckView
+from .views import LoginView, LogoutView, VerifyLoginState
 from rest_framework_simplejwt.views import TokenVerifyView
 
 
 urlpatterns = [
-    path('', view=HealthCheckView.as_view(), name='health-check'),
     path('auth/user/register/', view=views.CreateUserView.as_view(), name='create-user'),
     path('users/list/', view=views.UsersListView.as_view(), name='list-users'),
     path("user/<str:user_id>/", view=views.UserDetailsView.as_view(), name='update-get-del'),
